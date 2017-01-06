bd6e9014ab44ebd0196f915850fdf16851ce8efc
Michael Sermersheim <m.sermersheim@gmail.com>
2017-01-06 12:02:41 -0800

    Passes all tests

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,14 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  constructor() {
+    super();
+    this.age = 5
+    this.color = 'yellow'
+    this.job = 'Keep on growing'
+  }
 };
+
+//Grub superclass
+
+//method inherited from Grub
+
+//class, extends, and super keywords
